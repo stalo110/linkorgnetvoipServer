450e67c995db32e78aeb0c1809c8c14c25652350
Stanley Offor <offorstanley4real@gmail.com>
2024-10-29 15:39:24 +0100

    user model created

--- dist/app.js
+++ dist/app.js
@@ -11,9 +11,6 @@ const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("./routes/index"));
 const users_1 = __importDefault(require("./routes/users"));
 const app = (0, express_1.default)();
-// view engine setup
-app.set('views', path_1.default.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use((0, morgan_1.default)('dev'));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
